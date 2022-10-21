e80539f2b3a7a60f3697f6d1623e1ce30b2c91f6
Rodolfo <rodolfougaldeochoa@gmail.com>
2022-10-21 10:32:50 -0500

    :building_construction: [ADD] Configuration nginx.
    Se agrego configuracion de nginx.

--- config/settings/local.py
+++ config/settings/local.py
@@ -19,6 +19,9 @@ SECRET_KEY = env(
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
+# https://docs.djangoproject.com/en/dev/ref/settings/#csrf-trusted-origins
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8080']
+
 # CACHES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#caches
